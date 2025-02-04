9e8a031ad06491cc7d8a0d4753da5409cd6fab97
Johnny4251 <johnangus425@gmail.com>
2025-02-04 14:57:21 -0500

    terrible code

---
+++ morse/client.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define PORT 12345
+
+struct morse_map {
+    char letter;
+    const char *code;
+};
+
+struct morse_map morse_table[] = {
+    {'A', "o-"},    {'B', "-ooo"},  {'C', "-o-o"},  {'D', "-oo"},
+    {'E', "o"},     {'F', "oo-o"},  {'G', "--o"},   {'H', "oooo"},
+    {'I', "oo"},    {'J', "o---"},  {'K', "-o-"},   {'L', "o-oo"},
+    {'M', "--"},    {'N', "-o"},    {'O', "---"},   {'P', "o--o"},
+    {'Q', "--o-"},  {'R', "o-o"},   {'S', "ooo"},   {'T', "-"},
+    {'U', "oo-"},   {'V', "ooo-"},  {'W', "o--"},   {'X', "-oo-"},
+    {'Y', "-o--"},  {'Z', "--oo"},
+    {0, NULL}
+};
+
+char lookup_letter(const char *code) {
+    for (int i = 0; morse_table[i].letter; i++) {
+        if (strcmp(morse_table[i].code, code) == 0)
+            return morse_table[i].letter;
+    }
+    return '?';  // unknown 
+}
+
+int main(int argc, char **argv) {
+    int sockfd;
+    struct sockaddr_in server_addr, my_addr;
+    socklen_t addr_len = sizeof(server_addr);
+    char recv_char;
+    char token[12];
+    int token_index = 0;
+    
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+         perror("socket");
+         exit(EXIT_FAILURE);
+    }
+    
+    memset(&my_addr, 0, sizeof(my_addr));
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_addr.s_addr = INADDR_ANY;
+    my_addr.sin_port = 0;
+    
+    if (bind(sockfd, (struct sockaddr*)&my_addr, sizeof(my_addr)) < 0) {
+         perror("bind");
+         close(sockfd);
+         exit(EXIT_FAILURE);
+    }
+    
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port   = htons(PORT);
+
+    const char *init_msg = "Hello";
+    if (sendto(sockfd, init_msg, strlen(init_msg), 0,
+               (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+         perror("sendto");
+         close(sockfd);
+         exit(EXIT_FAILURE);
+    }
+    
+    struct timeval tv = {0, 1};
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
+         perror("setsockopt");
+         close(sockfd);
+         exit(EXIT_FAILURE);
+    }
+
+    printf("Waiting for message from server:\n");
+    while (recvfrom(sockfd, &recv_char, 1, 0, NULL, NULL) > 0) {
+         if (recv_char == ' ') {
+
+             if (token_index == 0) { printf(" "); } 
+             else {
+                 token[token_index] = '\0';
+                 printf("%c", lookup_letter(token));
+                 token_index = 0; 
+             }
+
+         } else if (recv_char == '-' || recv_char == 'o') {
+             token[token_index++] = recv_char;
+         } else {
+             token[token_index++] = recv_char;
+         }
+    }
+    
+    printf("\n");
+    close(sockfd);
+    return 0;
+}
+
---
+++ morse/server.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define PORT 12345
+
+struct morse_map {
+    char letter;
+    const char *code;
+};
+
+struct morse_map morse_table[] = {
+    {'A', "o-"},    {'B', "-ooo"},  {'C', "-o-o"},  {'D', "-oo"},
+    {'E', "o"},     {'F', "oo-o"},  {'G', "--o"},   {'H', "oooo"},
+    {'I', "oo"},    {'J', "o---"},  {'K', "-o-"},   {'L', "o-oo"},
+    {'M', "--"},    {'N', "-o"},    {'O', "---"},   {'P', "o--o"},
+    {'Q', "--o-"},  {'R', "o-o"},   {'S', "ooo"},   {'T', "-"},
+    {'U', "oo-"},   {'V', "ooo-"},  {'W', "o--"},   {'X', "-oo-"},
+    {'Y', "-o--"},  {'Z', "--oo"},
+    {0, NULL}
+};
+
+const char *lookup_morse(char letter) {
+    for (int i = 0; morse_table[i].letter != 0; i++) {
+        if (morse_table[i].letter == letter)
+            return morse_table[i].code;
+    }
+    return NULL;
+}
+
+int main(int argc, char **argv) {
+
+    if(argc < 2) {
+        printf("Usage: %s <command_string>\n", argv[0]);
+        return 1;
+    }
+
+    int sockfd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    char buf;
+    
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family      = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port        = htons(PORT);
+    
+    if (bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+         perror("bind");
+         close(sockfd);
+         exit(EXIT_FAILURE);
+    }
+    
+    printf("Server is running and waiting for a client...\n");
+
+    /* Main Loop */
+    while(1) {
+        if (recvfrom(sockfd, &buf, 1, 0, (struct sockaddr*)&client_addr, &client_addr_len) < 0) {
+            perror("recvfrom");
+            close(sockfd);
+            exit(EXIT_FAILURE);
+        }
+        printf("Received initial message from client.\n");
+        
+        
+        for (size_t i = 0; i < strlen(argv[1]); i++) {
+            char ch = argv[1][i];
+            if (ch == ' ') {
+                buf = ' ';
+                if (sendto(sockfd, &buf, 1, 0, (struct sockaddr*)&client_addr, client_addr_len) < 0)
+                    perror("sendto");
+            } else {
+                ch = toupper(ch);
+                const char *code = lookup_morse(ch);
+                if (code != NULL) {
+                    for (size_t j = 0; j < strlen(code); j++) {
+                        buf = code[j]; 
+                        if (sendto(sockfd, &buf, 1, 0, (struct sockaddr*)&client_addr, client_addr_len) < 0)
+                            perror("sendto");
+                    }
+                }
+                buf = ' ';
+                if (sendto(sockfd, &buf, 1, 0, (struct sockaddr*)&client_addr, client_addr_len) < 0)
+                    perror("sendto");
+            }
+        }
+    }
+    printf("Message sent.\n");
+    close(sockfd);
+    return 0;
+}
+
