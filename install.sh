ab85e33424ec989880f7d6b8e02636337321421d
johnny4251 <johnangus425@gmail.com>
2025-02-25 13:30:48 -0500

    Install script for labs

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ~
+mkdir -p ~/python_packages/
+mkdir -p ~/ollama_whl/
+
+cd ~/ollama_whl
+pip download ollama
+
+for file in *.whl; do
+    unzip -o "$file" -d .
+done
+
+export PYTHONPATH=$HOME/python_packages:$PYTHONPATH
+echo 'export PYTHONPATH=${pwd}/python_packages:$PYTHONPATH' >> ~/.bashrc
+source ~/.bashrc
+
