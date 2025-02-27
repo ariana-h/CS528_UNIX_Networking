4e8bd0e33ce0f8ec9b840ec9480389c4720f944c
Ariana Huhko <huhkoa2@sunypoly.edu>
2025-02-27 15:49:51 -0500

    using threads

--- llamas/convo.py
+++ llamas/convo.py
@@ -1,45 +1,53 @@
 #!/usr/bin/env python3
 
+import threading
+import time
 from ollama import Client
 
-model_name = "llama3.2"
-host_model = "llama3.1"
+
+model_1 = "llama3.2"
+model_2 = "llama3.1"
 host_addr = "localhost"
 
-def create_client(str):
-    return Client(host=f'http://{host_addr}:11434')
+def create_client(name, addr):
+    return Client(host=f'http://{addr}:11434')
 
 def chat_with_model(client, model_name, messages):
     try:
         response = client.chat(model=model_name, messages=messages)
-        if 'message' in response: return response['message']['content']
-        else: print("response format:", response)
+        return response['message']['content'] if "message" in response else ""
     except Exception as e: print(f"{e}")
 
-    return ""
+def model_convo(client, model_name, messages):
+    response = chat_with_model(client, model_name, messages)
+    if response:
+        print(f"[{model_name}]{response}")
+        messages.append({'role': 'user', 'content': response})
+        
 
 def main():
-    client = create_client(host_addr)
-    host = create_client("localhost")
-
+    client = create_client(model_1, "localhost")
+    host = create_client(model_2, host_addr)
     messages = []
     init_input = input(f"Initial Input: ")
+    messages.append({'role': 'user', 'content': init_input})
+    
     while True:
         if init_input.lower() == 'exit':
             print("Goodbye!")
             break
+        
+        thread1 = threading.Thread(target=model_convo, args=(client, model_1, messages))
+        thread2 = threading.Thread(target=model_convo, args=(host, model_2, messages))
 
-        messages.append({'role': 'user', 'content': init_input})
-        response = chat_with_model(client, model_name, messages)
-        if response:
-            print(f"{response}")
-            messages.append({'role': 'assistant', 'content': response})
+        thread1.start()
+        thread1.join()
 
-        response = chat_with_model(host, host_model, messages)
-        if response:
-            print(f"{response}")
-            messages.append({'role': 'assistant', 'content': response})
+        thread2.start()
+        thread2.join()
 
+        #time.sleep(1)
+            
 if __name__ == "__main__":
     main()
 
